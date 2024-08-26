9af7cce7bfb4ab52236566db82939ad69e017338
RohitSth <shrestha.rohit655@gmail.com>
2024-08-26 10:48:32 +0545

    update :: changed to modular type and used port in .env

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     // root route http://localhost:5000/
     res.send("Hello Okay");
 });
 
-app.listen(5000, () => console.log("Server is running in port 5000"));
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
