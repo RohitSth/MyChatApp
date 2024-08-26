2ab6afbfd4b777fd84c2326ad0d00ebe417f5a7f
RohitSth <shrestha.rohit655@gmail.com>
2024-08-26 19:11:05 +0545

    update :: send message and route protected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -10,7 +13,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json()); // to parse the incomming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDb();
