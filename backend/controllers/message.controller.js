bf07a47496a04c731b2c596a3fffaaad0c7359bf
RohitSth <shrestha.rohit655@gmail.com>
2024-08-26 19:24:32 +0545

    update :: get message endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -28,12 +28,34 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+
     // This will run in parallel
     await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessage controller: ", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
