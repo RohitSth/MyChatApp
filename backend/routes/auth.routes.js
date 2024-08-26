592296fc4e8ca641a20d98b32fcc1ecce7f4a17e
RohitSth <shrestha.rohit655@gmail.com>
2024-08-26 11:07:08 +0545

    update :: made a controller to handle routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,18 +1,11 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 // Auth routes
-router.get("/login", (req, res) => {
-    res.send("Login Route");
-});
-
-router.get("/signup", (req, res) => {
-    res.send("Signup route");
-});
-
-router.get("/logout", (req, res) => {
-    res.send("Logout route");
-});
+router.get("/signup", signup); 
+router.get("/login", login);
+router.get("/logout", logout);
 
 export default router;
