51fee6c836ece9b0c68977c63bf9e4e56f40d714
RohitSth <shrestha.rohit655@gmail.com>
2024-08-26 12:01:52 +0545

    refactor :: optimized the code of saving the new user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -31,15 +31,20 @@ export const signup = async (req, res) => {
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
+    if (newUser) {
+      // Generate JWT token here
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      //   gender: newUser.gender,
-      profilePic: newUser.profilePic,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        //   gender: newUser.gender,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
