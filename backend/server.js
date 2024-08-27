033675c4ba83230930e67a77cb1ce93ff7956279
RohitSth <shrestha.rohit655@gmail.com>
2024-08-27 10:18:56 +0545

    update :: get users for sidebar endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDb from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -17,6 +19,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDb();
