c6353817dd674142d277f5fb93822c2856c89f25
RohitSth <shrestha.rohit655@gmail.com>
2024-08-27 12:34:11 +0545

    update :: minor updates on sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
   return (
-    <div className="p-2 flex">
+    <div className="p-5 flex">
       <div className="h-full p-2 flex flex-col rounded-lg overflow-hidden bg-transparent bg-clip-padding backdrop-filter backdrop-blur-lg">
         <SearchInput />
         <div className="divider px-2"></div>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
-    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-white/20 bg-clip-padding backdrop-filter backdrop-blur-lg">
+    <div className="flex sm:h-[450px] md:h-[550px] xl:h-[700px] rounded-lg overflow-hidden bg-white/20 bg-clip-padding backdrop-filter backdrop-blur-lg">
       <Sidebar />
       {/* <MessageContainer /> */}
     </div>
