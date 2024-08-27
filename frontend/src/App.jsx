bd79c4ecff3076638274046667565dce188be577
RohitSth <shrestha.rohit655@gmail.com>
2024-08-27 11:17:53 +0545

    update :: signup UI done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import Login from "./pages/login/login";
+// import Login from "./pages/login/login";
+import SignUp from "./pages/signup/signup";
 
 function App() {
   return (
     <>
       <div className="relative p-4 h-screen flex items-center justify-center">
         <h1 className="absolute top-4 left-4 font-bold text-xl">HICHAT</h1>
-        <Login />
+        <SignUp />
       </div>
     </>
   );
