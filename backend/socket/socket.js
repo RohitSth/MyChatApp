dcebf13b7b7d89b575399c66b49aa07956f744af
RohitSth <shrestha.rohit655@gmail.com>
2024-08-28 13:53:32 +0545

    update :: implemented socket in the sidebar

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,12 +12,26 @@ const io = new Server(server, {
   },
 });
 
+const userSocketMap = {}; // {userId, socketId} : key value pair to store the mapping of userId and socketId
+
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
+
+  const userId = socket.handshake.query.userId; // userId is sent from the client side
+  if (userId != "undefined") {
+    userSocketMap[userId] = socket.id; // store the mapping of userId and socketId
+  }
+
+  // io.emit() is used to send the events to all the connected clients.
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   // socket.on() is used to listen for the events sent by the client. Can be used both on the server and client side.
   socket.on("disconnect", () => {
     console.log("User disconnected", socket.id);
+    delete userSocketMap[userId]; // delete the mapping of userId and socketId
+
+    // io.emit() is used to send the events to all the connected clients.
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./global.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>
