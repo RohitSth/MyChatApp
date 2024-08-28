226621a449a4fea3e378ea217820bc80b3c50c5e
RohitSth <shrestha.rohit655@gmail.com>
2024-08-28 15:54:05 +0545

    update :: changed some codes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -8,6 +9,8 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDb from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -20,6 +23,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDb();
