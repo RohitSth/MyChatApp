be9fa65d1d4b894ac966a2702b565008a7862e9b
RohitSth <shrestha.rohit655@gmail.com>
2024-08-28 12:21:57 +0545

    update :: minor update in the frontend

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,10 +3,13 @@ import useConversation from "../../zustand/useConversation";
 import MessageHeader from "./MessageHeader";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
-import { AiOutlineMessage } from "react-icons/ai";
+import { TbMessageChatbotFilled } from "react-icons/tb";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const { authUser } = useAuthContext();
 
   useEffect(() => {
     // Cleanup function (unmount)
@@ -17,11 +20,14 @@ const MessageContainer = () => {
     <div className="py-5 md:pt-5 md:pb-5 px-5 md:pl-0 md:pr-5 flex w-full  h-full">
       <div className="size-full p-2 flex flex-col rounded-2xl overflow-hidden bg-transparent bg-clip-padding backdrop-filter backdrop-blur-lg">
         {!selectedConversation ? (
-          <NoChatSelected />
+          <NoChatSelected authUser={authUser} />
         ) : (
           <>
-            <MessageHeader selectedConversation={selectedConversation} />
-            <Messages />
+            <MessageHeader
+              selectedConversation={selectedConversation}
+              authUser={authUser}
+            />
+            <Messages authUser={authUser} />
             <div className="mt-auto">
               <MessageInput />
             </div>
@@ -34,20 +40,20 @@ const MessageContainer = () => {
 
 export default MessageContainer;
 
-const NoChatSelected = () => {
+const NoChatSelected = ({ authUser }) => {
   return (
     <div className="relative flex size-full items-center justify-center">
       <div className="avatar absolute top-1 right-1 items-center">
         <div className="ring-white/40 w-8 rounded-full ring-1">
-          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          <img src={authUser.profilePic} />
         </div>
       </div>
       <div className="px-6 py-8 text-center text-white font-medium flex flex-col items-center gap-4">
-        <p className="text-xl md:text-2xl">Welcome, John Doe</p>
+        <p className="text-xl md:text-2xl">Welcome, {authUser.fullName}</p>
         <p className="text-sm md:text-lg text-white">
           Select a chat to start a conversation
         </p>
-        <AiOutlineMessage className="text-4xl md:text-6xl text-blue-500 mt-4" />
+        <TbMessageChatbotFilled className="text-4xl md:text-6xl text-white mt-4" />
       </div>
     </div>
   );
--- frontend/src/components/messages/MessageHeader.jsx
+++ frontend/src/components/messages/MessageHeader.jsx
@@ -1,6 +1,6 @@
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 
-const MessageHeader = ({ selectedConversation }) => {
+const MessageHeader = ({ selectedConversation, authUser }) => {
   return (
     <div className="bg-white/20 pl-2 pr-1 py-1 mb-2 rounded-full flex justify-between items-center">
       <div>
@@ -11,7 +11,7 @@ const MessageHeader = ({ selectedConversation }) => {
       </div>
       <div className="avatar">
         <div className="ring-white/40 w-7 rounded-full ring-1">
-          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          <img src={authUser.profilePic} />
         </div>
       </div>
     </div>
