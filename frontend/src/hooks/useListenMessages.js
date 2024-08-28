3edd954fdb0eaf0291dc87261c310dab02cc4875
RohitSth <shrestha.rohit655@gmail.com>
2024-08-28 14:20:42 +0545

    update :: added a sound notification for the new message

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
+import notificationSound from "../assets/sounds/notification.mp3";
+
 const useListenMessages = () => {
   const { socket } = useSocketContext();
   const { messages, setMessages } = useConversation();
@@ -9,6 +11,8 @@ const useListenMessages = () => {
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
       newMessage.shouldNudge = true;
+      const sound = new Audio(notificationSound);
+      sound.play();
       setMessages([...messages, newMessage]);
     });
     return () => {
